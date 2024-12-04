94d28cac3e75abb8f0fd997530785e18bce379fd
nooneknowspeter <jerelepeter06@gmail.com>
2024-12-04 01:39:43 -0400

    installed @d3/types

--- src/app/components/HeatMapGraph/HeatMapGraph.tsx
+++ src/app/components/HeatMapGraph/HeatMapGraph.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import * from "d3"
 
 const HeatMapGraph = () => {
-  return <div>HeatMapGraph</div>;
+  const chartHeatMap = async () => {
+    const data = await d3.json("example.json");
+  };
+
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <h1 id="title" className="text-center">
+          title
+        </h1>
+        <div id="axes"></div>
+      </div>
+    </>
+  );
 };
 
 export default HeatMapGraph;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { HeatMapGraph } from "./components/Components";
 export default function Home() {
   return (
     <>
-      <h1>hello world</h1>
       <HeatMapGraph />
     </>
   );
