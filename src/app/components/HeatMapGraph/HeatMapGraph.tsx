67477be18f95fde7f91d2c632b12fcf90d778ccb
nooneknowspeter <jerelepeter06@gmail.com>
2024-12-04 01:52:24 -0400

    added svg canvas to div

--- src/app/components/HeatMapGraph/HeatMapGraph.tsx
+++ src/app/components/HeatMapGraph/HeatMapGraph.tsx
@@ -14,11 +14,24 @@ const HeatMapGraph = () => {
     const data = await d3.json(url);
 
     console.log(data);
+
+    const width = 1000;
+    const height = 600;
+    const marginTop = 20;
+    const marginRight = 20;
+    const marginBottom = 20;
+    const marginLeft = 20;
+
+    const svg = d3
+      .select("#axes")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
   };
 
   return (
     <>
-      <div className="flex h-screen items-center justify-center">
+      <div className="flex h-screen flex-col items-center justify-center">
         <h1 id="title" className="text-center">
           title
         </h1>
