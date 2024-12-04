bfe19871cce467b44b84a9cf6f9f2adc6ca7545e
nooneknowspeter <jerelepeter06@gmail.com>
2024-12-04 13:19:01 -0400

    added x and y axis

--- src/app/components/HeatMapGraph/HeatMapGraph.tsx
+++ src/app/components/HeatMapGraph/HeatMapGraph.tsx
@@ -8,6 +8,7 @@ const HeatMapGraph = () => {
   });
 
   const chartHeatMap = async () => {
+    // provided json data containing global land temperature since 1753
     const url =
       "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 
@@ -15,18 +16,80 @@ const HeatMapGraph = () => {
 
     console.log(data);
 
-    const width = 1000;
-    const height = 600;
-    const marginTop = 20;
-    const marginRight = 20;
-    const marginBottom = 20;
-    const marginLeft = 20;
+    // 1.92 ratio
+    const width = 1344;
+    const height = 700;
 
+    // margin
+    const marginTop = 20;
+    const marginRight = 40;
+    const marginBottom = 30;
+    const marginLeft = 80;
+
+    // create and append svg canvas
     const svg = d3
       .select("#axes")
       .append("svg")
       .attr("width", width)
       .attr("height", height);
+
+    // x axis
+    const x = d3
+      .scaleBand()
+      .domain(
+        data.monthlyVariance.map((el) => {
+          // console.log(el.year);
+          return el.year;
+        }),
+      )
+      .range([marginLeft, width - marginRight]);
+
+    const xAxis = d3
+      .axisBottom(x)
+      .tickValues(
+        x.domain().filter((year) => {
+          // set ticks to years divisible by 10
+          return year % 10 === 0;
+        }),
+      )
+      .tickFormat((year) => {
+        const date = new Date(0);
+        date.setUTCFullYear(year);
+        const format = d3.utcFormat("%Y");
+        return format(date);
+      })
+      .tickSize(10, 1);
+
+    svg
+      .append("g")
+      .attr("id", "x-axis")
+      .attr("transform", `translate(0,${height - marginBottom})`)
+      .call(xAxis);
+
+    // y axis
+    const y = d3
+      .scaleBand()
+      // months
+      .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+      .rangeRound([height - marginBottom, marginTop]);
+
+    const yAxis = d3
+      .axisLeft(y)
+      .tickValues(y.domain())
+      .tickFormat((month) => {
+        const date = new Date(0);
+        date.setUTCMonth(month);
+        const format = d3.utcFormat("%B");
+        return format(date);
+      })
+      .tickSize(10, 1);
+
+    svg
+      .append("g")
+      .classed("y-axis", true)
+      .attr("id", "y-axis")
+      .attr("transform", `translate(${marginLeft},0)`)
+      .call(yAxis);
   };
 
   return (
